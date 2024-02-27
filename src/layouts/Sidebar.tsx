161adca253ea355b05bc25a6e2452df67d3e00c7
Faith Sabu <faithsabu1791@gmail.com>
2024-02-27 12:40:16 +0530

    Implement toggling sidebar

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -26,11 +26,19 @@ import { Button, buttonStyles } from "../components/Button";
 import { twMerge } from "tailwind-merge";
 import {} from "lucide-react";
 import { playlists, subscriptions } from "../data/sidebar";
+import { useSidebarContext } from "../context/SidebarContext";
+import { PageHeaderFirstSection } from "./PageHeader";
 
 export function Sidebar() {
+  const { isLargeOpen, isSmallOpen, close } = useSidebarContext();
+
   return (
     <>
-      <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden">
+      <aside
+        className={`sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 ${
+          isLargeOpen ? "lg:hidden" : "lg:flex"
+        }`}
+      >
         <SmallSidebarItem Icon={Home} title="Home" url="/" />
         <SmallSidebarItem Icon={Repeat} title="Shorts" url="/shorts" />
         <SmallSidebarItem
@@ -40,7 +48,20 @@ export function Sidebar() {
         />
         <SmallSidebarItem Icon={Library} title="Library" url="/library" />
       </aside>
-      <aside className="w-56 lg:sticky absolute tip-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 hidden lg:flex">
+      {isSmallOpen && (
+        <div
+          onClick={close}
+          className="lg:hidden fixed inset-0 z-[999] bg-secondary-dark opacity-50"
+        />
+      )}
+      <aside
+        className={`w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 ${
+          isLargeOpen ? "lg:flex" : "lg:hidden"
+        } ${isSmallOpen ? "flex z-[999] bg-white max-h-screen" : "hidden"}`}
+      >
+        <div className="lg:hidden pt-2 pb-4 px-2 sticky top-0 bg-white">
+          <PageHeaderFirstSection />
+        </div>
         <LargeSidebarSection visibleItemCount={1}>
           <LargeSidebarItem isActive IconOrImgUrl={Home} title="Home" url="/" />
           <LargeSidebarItem
